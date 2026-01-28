a28c38fb4d33fcfcb88f3e74332cca8f1d686569
leila-boushehri-SWW <120033581+leila-boushehri-SWW@users.noreply.github.com>
2026-01-28 16:59:05 +0000

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [require.resolve("babel-preset-expo")],
-    plugins: [require.resolve("nativewind/babel")],
+    plugins: [],
   };
 };
