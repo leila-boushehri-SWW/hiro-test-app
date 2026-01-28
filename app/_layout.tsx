f437956ce8e02279e643b8ab233f2f53f206f570
leila-boushehri-SWW <120033581+leila-boushehri-SWW@users.noreply.github.com>
2026-01-28 14:42:58 +0000

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "../global.css";
 
 export default function RootLayout() {
   return (
